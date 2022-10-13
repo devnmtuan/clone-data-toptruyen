9ca3a49b587b1c5437104e1f37d0739e435ebf14
devshynhgroup <99156603+devshynhgroup@users.noreply.github.com>
2022-10-13 07:40:54 +0700

    x5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const schedule = require("node-schedule")
 // const nettruyen = require('../nettruyen');
 const toptruyen = require('./toptruyen');
 
-schedule.scheduleJob('0 */30 * * * *', async function () {
+schedule.scheduleJob('0 */5 * * * *', async function () {
     await toptruyen();
 });
 
